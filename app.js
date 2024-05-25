9b4dbea51ab663c68b336aa4b8ec0f128b95684a
Shumimran <164326671+Shumimran@users.noreply.github.com>
2024-05-25 11:38:04 +0500

    titlecase
    this is an assignment

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+// change the lowercase into title case
+let FirstName = "shumaila";
+console.log(FirstName[0].toUpperCase() + FirstName.slice(1));
---
+++ app.ts
@@ -0,0 +1,9 @@
+// change the lowercase into title case
+
+
+
+let FirstName:string = "shumaila";
+
+console.log(FirstName[0] .toUpperCase() + FirstName.slice(1));
+
+
